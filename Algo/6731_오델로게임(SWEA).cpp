482fd31aa7378df321d777dd273554783a058515
한동호 <gksehdgh94@gmail.com>
2019-03-22 17:23:01 +0900

    시뮬레이션?... 규칙찾기 너무 어렵다.. 외워야하나..?

--- Algo/6731_오델로게임(SWEA).cpp
+++ Algo/6731_오델로게임(SWEA).cpp
@@ -1,35 +1,57 @@
 #pragma warning(disable : 4996)
 
 #include <cstdio>
-#include <vector>
 #include <memory.h>
-
 #define MAX 1001
-
-
 using namespace std;
 
-int ans[MAX][MAX];
-int map[MAX][MAX];
+int N;
+char map[MAX][MAX];
+int row[MAX];
+int col[MAX];
 
-
+void init()
+{
+	memset(map, 0, sizeof(map));
+	memset(row, 0, sizeof(row));
+	memset(col, 0, sizeof(col));
+}
 
 int main(void)
 {
 	int T;
 	scanf("%d", &T);
-	
+
 	for (int tc = 1; tc <= T; tc++) {
-		int N;
+		init();
 		scanf("%d", &N);
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				scanf(" %c", &ans[i][j]);
+				scanf(" %c", &map[i][j]);
+				if (map[i][j] == 'B')
+				{
+					row[i]++;
+					col[j]++;
+				}
 			}
 		}
 
+		int cnt = 0;
+		
 
+		// y=x 대칭축을 기준으로 
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				int blackstone = row[i] + col[j];
+				if (map[i][j] == 'B')
+					blackstone--;
+				if (blackstone % 2 == 1) //홀수이면 cnt++
+					cnt++;
+			}
+		}
 
-
+		printf("#%d %d\n", tc, cnt);
 	}
+	return 0;
 }
