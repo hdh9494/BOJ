fe5cb1abba8bfb673a0ef94dfc60f24eee8c0937
한동호 <gksehdgh94@gmail.com>
2019-03-28 14:12:52 +0900

    DFS 재귀 방법 / 배열과 함수 배치를 통한 재귀

---
+++ Algo/기출_14888_연산자끼워넣기.cpp
@@ -0,0 +1,76 @@
+#pragma warning(disable : 4996)
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int op[4];
+
+int min_sol = 999999999;
+int max_sol = -999999999;
+
+vector <int> num;
+
+void dfs(int total, int cnt)
+{
+	if (cnt == N)
+	{
+		min_sol = min(min_sol, total);
+		max_sol = max(max_sol, total);
+		return;
+	}
+
+	if (op[0] > 0)
+	{
+		op[0]--;
+		dfs(total + num[cnt], cnt + 1);
+		op[0]++;
+	}
+
+	if (op[1] > 0)
+	{
+		op[1]--;
+		dfs(total - num[cnt], cnt + 1);
+		op[1]++;
+	}
+
+	if (op[2] > 0)
+	{
+		op[2]--;
+		dfs(total * num[cnt], cnt + 1);
+		op[2]++;
+	}
+
+	if (op[3] > 0)
+	{
+		op[3]--;
+		dfs(total / num[cnt], cnt + 1);
+		op[3]++;
+	}
+
+}
+
+int main(void)
+{
+	int val;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &val);
+		num.push_back(val);
+	}
+
+	for (int i = 0; i < 4 ; i++) {
+		scanf("%d", &val);
+		op[i] = val;
+	}
+
+
+	dfs(num[0], 1);
+
+	printf("%d\n", max_sol);
+	printf("%d\n", min_sol);
+	return 0;
+}
