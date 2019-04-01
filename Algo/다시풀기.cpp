df5737b5306d2feee9aa0208958d9d618164e4bd
한동호 <gksehdgh94@gmail.com>
2019-04-01 16:47:56 +0900

    로봇청소기 복습

--- Algo/다시풀기.cpp
+++ Algo/다시풀기.cpp
@@ -1,27 +1,126 @@
 #pragma warning(disable : 4996)
-#include <cstdio>
-#include <vector>
-#include <algorithm>
 
+#include <cstdio>
+#include <queue>
+
+#define MAX 50
 using namespace std;
+
+int N, M;
+int r, c, d;
+
+int map[MAX][MAX];
+
+int dx[4] = {};
+int dy[4] = { };
+
+void dfs(int x, int y, int dir)
+{
+	if (map[x][y] == 1)
+		return;
+
+	map[x][y] = 9;
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (dir == 0)
+		{
+			dir = 3;
+			int ny = y - 1;
+			if (map[x][ny] == 0) {
+				dfs(x, ny, dir);
+				return;
+			}
+
+			else
+				continue;
+		}
+
+
+		if (dir == 3)
+		{
+			dir = 2;
+			int nx = x + 1;
+			if (map[nx][y] == 0) {
+				dfs(nx, y, dir);
+				return;
+			}
+			else
+				continue;
+		}
+
+		if (dir == 2)
+		{
+			dir = 1;
+			int ny = y + 1;
+			if (map[x][ny] == 0) {
+				dfs(x, ny, dir);
+				return;
+			}
+			else
+				continue;
+		}
+
+		if (dir == 1)
+		{
+			dir = 0;
+			int nx = x - 1;
+			if (map[nx][y] == 0) {
+				dfs(nx, y, dir);
+				return;
+			}
+			else
+				continue;
+		}
+	}
+	// Å½»ö ¿Ï·á
+
+	if (dir == 0)
+	{
+		int nx = x + 1;
+		dfs(nx, y, dir);
+	}
+
+	if (dir == 3)
+	{
+		int ny = y + 1;
+		dfs(x, ny, dir);
+	}
+
+	if (dir == 2)
+	{
+		int nx = x - 1;
+		dfs(nx, y, dir);
+	}
+
+	if (dir == 1)
+	{
+		int ny = y - 1;
+		dfs(x, ny, dir);
+	}
+	
+}
 
 int main(void)
 {
-	vector <int> vc;
-	
-	int N, val;
-	scanf("%d", &N);
+	scanf("%d %d", &N, &M);
+	scanf("%d %d %d", &r, &c, &d);
 
-	for(int i= 0 ; i<N ; i++){
-		scanf("%d", &val);
-		vc.push_back(val);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scanf("%d", &map[i][j]);
+		}
 	}
-
-	sort(vc.begin(), vc.end());
-
-	for (int i = 0; i < vc.size(); i++)
-		printf("%d\n", vc[i]);
-
+	
+	dfs(r, c, d);
+	
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (map[i][j] == 9)
+				cnt++;
+		}
+	}
+	printf("%d\n", cnt);
 	return 0;
-		
 }
