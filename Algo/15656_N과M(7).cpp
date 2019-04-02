90ad1216604205dec714a21c7dc35a57126a572e
한동호 <gksehdgh94@gmail.com>
2019-04-03 01:09:22 +0900

    중복순열

---
+++ Algo/15656_N과M(7).cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable : 4996)
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#define MAX 10
+
+using namespace std;
+
+int N, M;
+int arr[MAX];
+vector <int> vc;
+
+void dfs(int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < vc.size(); i++)
+			printf("%d ", vc[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		vc.push_back(arr[i]);
+		dfs(cnt + 1);
+		vc.pop_back();
+	}
+}
+
+int main(void)
+{
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+
+	sort(arr, arr + N);
+
+	dfs(0);
+	return 0;
+}
