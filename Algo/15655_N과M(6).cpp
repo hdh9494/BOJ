c5ef62fbf453f1237a5c24cf39a703965f98a705
한동호 <gksehdgh94@gmail.com>
2019-04-03 01:07:08 +0900

    조합 // dfs(0,0) 파라미터 위치 잘 보기

---
+++ Algo/15655_N과M(6).cpp
@@ -0,0 +1,43 @@
+#pragma warning(disable : 4996)
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#define MAX 10
+
+using namespace std;
+
+int N, M;
+int arr[MAX];
+vector <int> vc;
+
+void dfs(int num, int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < vc.size(); i++)
+			printf("%d ", vc[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = num; i < N; i++)
+	{
+		vc.push_back(arr[i]);
+//		dfs(num + 1, cnt + 1)  구분하기.
+		dfs(i + 1, cnt + 1);
+		vc.pop_back();
+	}
+}
+
+int main(void)
+{
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+
+	sort(arr, arr + N);
+
+	dfs(0, 0);
+	return 0;
+}
