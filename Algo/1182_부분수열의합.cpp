e673b26fb112f05e4492003089ee1f9c924c2e2f
한동호 <gksehdgh94@gmail.com>
2019-04-04 01:18:36 +0900

    완탐 + DFS (퇴사랑 비슷)

---
+++ Algo/1182_부분수열의합.cpp
@@ -0,0 +1,41 @@
+#pragma warning(disable : 4996)
+
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int N, S;
+int sol;
+
+vector <int> num;
+
+void dfs(int amount, int cnt)
+{
+	if (cnt == N)
+	{
+		if (amount == S)
+			sol++;
+		return;
+	}
+
+	dfs(amount + num[cnt], cnt + 1);
+	dfs(amount, cnt + 1);
+}
+
+int main(void)
+{
+	int val;
+	scanf("%d %d", &N, &S);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &val);
+		num.push_back(val);
+	}
+	
+	dfs(0,0);
+
+
+
+	printf("%d\n", sol);
+	return 0;
+}
