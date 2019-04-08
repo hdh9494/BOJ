a1f7f7a1b0644082f1cf4b1ba430b6f4fe799bc5
한동호 <gksehdgh94@gmail.com>
2019-04-08 16:58:38 +0900

    중복조합

---
+++ Algo/15666_N과M(12).cpp
@@ -0,0 +1,47 @@
+#pragma warning(disable : 4996)
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> a, v;
+
+void dfs(int idx, int cnt)
+{
+	if (cnt == m) 
+	{
+		for (auto k : v) 
+			printf("%d ", k);
+		printf("\n");
+		return;
+	}
+
+	for (int i = idx; i < n; i++) 
+	{
+		v.push_back(a[i]);
+		dfs(i, cnt + 1);
+		v.pop_back();
+	}
+}
+
+int main(void)
+{
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++)
+	{
+		int t;
+		scanf("%d", &t);
+		if (find(a.begin(), a.end(), t) == a.end())
+			a.push_back(t);
+	}
+
+	sort(a.begin(), a.end());
+	n = a.size();
+
+	dfs(0, 0);
+
+	return 0;
+}
