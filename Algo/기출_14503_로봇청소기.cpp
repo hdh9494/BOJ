af269e331752b7c3ed9654c5f3591b2970def491
한동호 <gksehdgh94@gmail.com>
2019-04-10 17:03:32 +0900

    DFS(재귀) + 시뮬레이션 / 기저사례 return위치 확인 필수!!

--- Algo/기출_14503_로봇청소기.cpp
+++ Algo/기출_14503_로봇청소기.cpp
@@ -7,14 +7,25 @@
 using namespace std;
 
 int N, M;
-int f_x, f_y, f_dir;
+int f_x, f_y#include <cstdio>
+
+#define MAX 51
+using namespace std;
+
+int N, M;
+int r, c, d;
+int sol = 1;
 
 int map[MAX][MAX];
-int visit[MAX][MAX];
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+
+// 기저사례 return 잘 설정하기
 
 void dfs(int x, int y, int dir)
 {
-	// 벽을 만났을 때, return 추가!
+	// 후진해서 벽을 만났을 때, return 
 	if (map[x][y] == 1)
 		return;
 
@@ -24,60 +35,46 @@ void dfs(int x, int y, int dir)
 		{
 			dir = 3;
 			int ny = y - 1;
-			if (!map[x][ny]) {
-				map[x][ny] = 9;
-				dfs(x, ny, dir);
-				// return 위치 생각.
-				return;
-			}
-			else
-				// 벽이거나 이미 청소한 위치면, 방향을 왼쪽으로 돌린다
-				continue;
+			if (map[x][ny] != 0) continue;
+			map[x][ny] = 9;
+			dfs(x, ny, dir);
 
+			// 이부분에 return 이 없으면
+			// 뒤로 돌아가서 for문 4번을 다 돈다
+			return;
 		}
 
 		else if (dir == 1)
 		{
 			dir = 0;
 			int nx = x - 1;
-			if (!map[nx][y]) {
-				map[nx][y] = 9;
-				dfs(nx, y, dir);
-				return;
-			}
-			else
-				continue;
-
+			if (map[nx][y] != 0) continue;
+			map[nx][y] = 9;
+			dfs(nx, y, dir);
+			return;
 		}
 
 		else if (dir == 2)
 		{
 			dir = 1;
 			int ny = y + 1;
-			if (!map[x][ny]) {
-				map[x][ny] = 9;
-				dfs(x, ny, dir);
-				return;
-			}
-			else
-				continue;
-
+			if (map[x][ny] != 0) continue;
+			map[x][ny] = 9;
+			dfs(x, ny, dir);
+			return;
 		}
 
 		else if (dir == 3)
 		{
 			dir = 2;
 			int nx = x + 1;
-			if (!map[nx][y]) {
-				map[nx][y] = 9;
-				dfs(nx, y, dir);
-				return;
-			}
-			else
-				continue;
+			if (map[nx][y] != 0) continue;
+			map[nx][y] = 9;
+			dfs(nx, y, dir);
+			return;
 		}
-	}// 검사 끝
-	
+	}
+
 
 	// 4방향이 모두 청소했거나, 방인 경우
 	// 방향을 유지한채로 후진.
@@ -104,15 +101,13 @@ void dfs(int x, int y, int dir)
 		int ny = y + 1;
 		dfs(x, ny, dir);
 	}
-	
 
 }
 
 int main(void)
 {
 	scanf("%d %d", &N, &M);
-	scanf("%d %d %d", &f_x, &f_y, &f_dir);
-
+	scanf("%d %d %d", &r, &c, &d);
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
@@ -120,8 +115,9 @@ int main(void)
 		}
 	}
 
-	map[f_x][f_y] = 9;
-	dfs(f_x, f_y, f_dir);
+
+	map[r][c] = 9;
+	dfs(r, c, d);
 
 	int cnt = 0;
 	for (int i = 0; i < N; i++) {
@@ -133,5 +129,4 @@ int main(void)
 
 	printf("%d\n", cnt);
 	return 0;
-
 }
