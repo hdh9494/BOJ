05cb9a8dfc0b0e27ca1ca001e810c8667c751cda
한동호 <gksehdgh94@gmail.com>
2019-05-12 22:39:38 +0900

    피보나치 - 1. 재귀 / 2. DP

--- Algo/2747_피보나치수.cpp
+++ Algo/2747_피보나치수.cpp
@@ -1,39 +1,45 @@
 #pragma warning(disable : 4996)
 
 #include <cstdio>
-/* TLE(Time Limit Exceed)
-int Fibo(int n)
-{
-	if (n == 0)
-		return 0;
 
-	if (n == 1)
+int dp[20];
+
+/* 1. 재귀로 구현
+int fibo(int num)
+{
+	if (num == 0)
+		return 0;
+	
+	if (num == 1)
 		return 1;
 
-	return Fibo(n - 2) + Fibo(n - 1);
+	return fibo(num - 2) + fibo(num - 1);
 }
 
 int main(void)
 {
-	int N;
-	scanf("%d", &N);
-	printf("%d\n", Fibo(N));
+	int n, sol;
+	scanf("%d", &n);
+
+	sol = fibo(n);
+	printf("%d\n", sol);
 	return 0;
 }
 */
+
+// 2. dp로 구현
 int main(void)
 {
-	int N;
-	scanf("%d", &N);
+	dp[0] = 0;
+	dp[1] = 1;
+	
+	int n;
+	scanf("%d", &n);
 
-	int A[1212];
+	for (int i = 2; i <= n; i++)
+		dp[i] = dp[i - 2] + dp[i - 1];
 
-	A[0] = 0;
-	A[1] = 1;
-
-	for (int i = 2; i <= N; i++) 
-		A[i] = A[i - 1] + A[i - 2];
-
-	printf("%d", A[N]);
+	printf("%d\n", dp[n]);
 	return 0;
+	
 }
