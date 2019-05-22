4e3be4d0f89c8c43a6964f44d9a03352704430a0
한동호 <gksehdgh94@gmail.com>
2019-05-22 16:41:26 +0900

    스택 응용

--- Algo/9012_괄호.cpp
+++ Algo/9012_괄호.cpp
@@ -34,3 +34,51 @@ int main(void)
 	}
 	return 0;
 }
+
+/*
+
+int main(void)
+{
+	int tc;
+	scanf("%d", &tc);
+
+	for (int i = 0; i < tc; i++)
+	{
+		int cnt1 = 0, cnt2 = 0;
+		bool flag = false;
+		stack <int> st;
+		char str[51] = { 0, };
+
+		scanf("%s", str);
+
+		for (int j = 0; str[j]; j++)
+		{
+			if (str[j] == '(')
+			{
+				st.push(1);
+				cnt1++;
+			}
+
+			else if (str[j] == ')')
+			{
+				if (st.empty()) {
+					flag = true;
+					break;
+				}
+
+				else {
+					st.pop();
+					cnt2++;
+				}
+			}
+		}
+
+		if (cnt1 == cnt2 && !flag)
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+	return 0;
+}
+
+*/
